9f0462801c6657dccdad83a109d9274a425d3173
Oscar Andres Ochoa Villegas <oscar.ochoavillegas@cj.com>
2025-11-18 20:58:53 -0500

    Added not inverted python example

--- dependency-inversion-python/src/file_util_not_inverted.py
+++ dependency-inversion-python/src/file_util_not_inverted.py
@@ -7,4 +7,4 @@ def concatenate_files(source1: Path, source2: Path, destination: Path) -> None:
     destination.write_bytes(concatenate_files)
 
 if __name__ == "__main__":
-    concatenate_files(Path("a.txt"), Path("b.txt"), Path("out.txt"))
+    concatenate_files(Path("a.txt"), Path("b.txt"), Path("out.txt"))1
